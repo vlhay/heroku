a2bb35293bf60d9bd6790cf8ad34bd994030f963
vlhay <bloggernamnguyen@gmail.com>
2021-08-31 06:56:06 +0700

    Update s1.php

--- s1.php
+++ s1.php
@@ -77,7 +77,45 @@ curl_close($curl);
 
 
 echo '
+<style>
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
 
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 140px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  bottom: 150%;
+  left: 50%;
+  margin-left: -75px;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.tooltip .tooltiptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+</style>
 <dd>
 
   <div class="js-prefixContainer">
@@ -110,16 +148,28 @@ echo '
 
       </div>
 
-    <input type="text" id="txtUrl2" value="Truyện Hentai '.$title.'" ">
-  <button onclick="geturl()">Copy</button>
+    <input type="text" id="myInput" value="Truyện Hentai '.$title.'" ">
+<div class="tooltip">
+<button onclick="myFunction()" onmouseout="outFunc()">
+  <span class="tooltiptext" id="myTooltip">Copy to clipboard</span>
+  Copy text
+  </button>
+</div>
+
 <script>
-function geturl(){
-  var copyText = document.getElementById("txtUrl2");
+function myFunction() {
+  var copyText = document.getElementById("myInput");
   copyText.select();
-  copyText.setSelectionRange(0, 99999)
-  document.execCommand("copy");
-  alert("Copy thành công.");
-  return false;
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
 }
 </script>
 
@@ -143,17 +193,7 @@ function geturl(){
     for ($i= 1; $i <= $lay ; $i++){ echo '[img]'.$ua.$i.'.jpg[/img]'; }
 
       echo '" type="text" />
-<button onclick="geturl()">Copy</button>
-<script>
-function geturl(){
-  var copyText = document.getElementById("txtUrl");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999)
-  document.execCommand("copy");
-  alert("Copy thành công.");
-  return false;
-}
-</script>
+
 
 
   <div class="js-previewContainer"></div>
