9c5fb5cbb4491b3352e8eee12718e15eb6954360
vlhay <bloggernamnguyen@gmail.com>
2021-09-04 20:10:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="vi">
+<head>
+<meta name="google-site-verification" content="7YgKSV2YcADqlo_7AIOiUmF5WPEuCPPe4l2lf1FiPa0" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="robots" content="all,index,follow" />
+<meta name="revisit-after" content="1 days" />
+<meta http-equiv="content-language" content="vi" />
+<meta name="google-site-verification" content="G8IJb6Box0zoBJqyzRiPuBZqcvm9_aMN1R-Vb0Jwdag" />
+<meta name="copyright" content="Copyright © 2019 TruyenHentai.XYZ" />
+<meta name="Author" content="MyGirl" />
+<meta name="keywords" content=",truyen hentai, hentaivn.net, hentailx.com, hentai24h.org, hentai full,anhsexdep.viwap.com, truyenhentai.viwap.com, truyenhentai.xyz"/>
+<meta name="description" content=". Đọc truyện full mọi thể loại hấp đãn Thế giới truyện hentai truyenhentai.viwap.com" />
+<title>Đọc Truyện Hentai Online - Truyện Tranh Sex Manga Hentai 18+ | TruyenHentai.Viwap.Com</title>
+<link rel="canonical" href="http://truyenhentai.xyz/" /> 
+<link rel="stylesheet" type="text/css" href="http://truyenhentai.xyz/css/truyenx.css" media="all,handheld" />
+<link rel="stylesheet" type="text/css" href="http://truyenhentai.xyz/css/style-th.css" media="all,handheld" />
+<link rel="stylesheet" type="text/css"  href="http://truyenhentai.xyz/css/top18.css" />
+<link rel="stylesheet" type="text/css"  href="http://truyenhentai.xyz/css/tab.css" />
+<link rel="shortcut icon" href="http://truyenhentai.viwap.com/images/favicon.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+</head><body>
+<div id="wrap"><div class="box"><div class="menu" id="posts">THÔNG BÁO</div>
+
+<div class="list"><a href="/s1">leech truyen xxx</a></div>
+<div class="list"><a href="/s1">leech truyen xxx</a></div>
+<div class="list"><a href="/s1">leech truyen xxx</a></div>
+<div class="list"><a href="/s1">leech truyen xxx</a></div>
+<div class="list"><a href="/s1">leech truyen xxx</a></div>
+<div class="list"><a href="/s1">leech truyen xxx</a></div>
+
+
+     </div>
+
+
+
+</div></body></html>
